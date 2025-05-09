eddaadff12ac2fc6bced68d906a93ffd6175fc0d
Kacper <cyrankowskikacper123@gmail.com>
2025-05-09 23:38:51 +0200

    zmiana Makefile na CMakeLists.txt

---
+++ build/CMakeFiles/mygame.dir/engine/renderer/camera/Camera.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/mygame.dir/engine/renderer/camera/Camera.cpp.o: \
+ /home/kacper/cpp/Engine2D/engine/renderer/camera/Camera.cpp \
+ /usr/include/stdc-predef.h \
+ /home/kacper/cpp/Engine2D/engine/renderer/camera/Camera.hpp
---
+++ build/CMakeFiles/mygame.dir/link.d
@@ -0,0 +1,199 @@
+mygame: \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/Scrt1.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/crti.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/crtbeginS.o \
+  CMakeFiles/mygame.dir/engine/core/Engine.cpp.o \
+  CMakeFiles/mygame.dir/engine/entity/Entity.cpp.o \
+  CMakeFiles/mygame.dir/engine/entity/enemy/Enemy.cpp.o \
+  CMakeFiles/mygame.dir/engine/entity/enemy/movementStrategy/simpleFollowStrategy/SimpleFollowStrategy.cpp.o \
+  CMakeFiles/mygame.dir/engine/entity/npc/Npc.cpp.o \
+  CMakeFiles/mygame.dir/engine/entity/player/Player.cpp.o \
+  CMakeFiles/mygame.dir/engine/entity/player/playerMovement/simplePlayerMovement/SimplePlayerMovement.cpp.o \
+  CMakeFiles/mygame.dir/engine/fps/Fps.cpp.o \
+  CMakeFiles/mygame.dir/engine/input/InputManager.cpp.o \
+  CMakeFiles/mygame.dir/engine/renderer/Renderer.cpp.o \
+  CMakeFiles/mygame.dir/engine/renderer/camera/Camera.cpp.o \
+  CMakeFiles/mygame.dir/game/Game.cpp.o \
+  CMakeFiles/mygame.dir/src/main.cpp.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libSDL2.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libSDL2_ttf.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libSDL2.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libSDL2_ttf.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libstdc++.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libm.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libm.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libm.so \
+  /usr/lib/libm.so.6 \
+  /usr/lib/libmvec.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libc.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libc.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libc.so \
+  /usr/lib/libc.so.6 \
+  /usr/lib/libc_nonshared.a \
+  /usr/lib/ld-linux-x86-64.so.2 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/crtendS.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/crtn.o \
+  /usr/lib32/libfreetype.so.6 \
+  /usr/lib/libfreetype.so.6 \
+  /usr/lib32/libharfbuzz.so.0 \
+  /usr/lib/libharfbuzz.so.0 \
+  /usr/lib/ld-linux-x86-64.so.2 \
+  /usr/lib32/libz.so.1 \
+  /usr/lib/libz.so.1 \
+  /usr/lib32/libbz2.so.1.0 \
+  /usr/lib/libbz2.so.1.0 \
+  /usr/lib32/libpng16.so.16 \
+  /usr/lib/libpng16.so.16 \
+  /usr/lib32/libbrotlidec.so.1 \
+  /usr/lib/libbrotlidec.so.1 \
+  /usr/lib32/libglib-2.0.so.0 \
+  /usr/lib/libglib-2.0.so.0 \
+  /usr/lib/libgraphite2.so.3 \
+  /usr/lib32/libbrotlicommon.so.1 \
+  /usr/lib/libbrotlicommon.so.1 \
+  /usr/lib32/libpcre2-8.so.0 \
+  /usr/lib/libpcre2-8.so.0
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/Scrt1.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/crti.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/crtbeginS.o:
+
+CMakeFiles/mygame.dir/engine/core/Engine.cpp.o:
+
+CMakeFiles/mygame.dir/engine/entity/Entity.cpp.o:
+
+CMakeFiles/mygame.dir/engine/entity/enemy/Enemy.cpp.o:
+
+CMakeFiles/mygame.dir/engine/entity/enemy/movementStrategy/simpleFollowStrategy/SimpleFollowStrategy.cpp.o:
+
+CMakeFiles/mygame.dir/engine/entity/npc/Npc.cpp.o:
+
+CMakeFiles/mygame.dir/engine/entity/player/Player.cpp.o:
+
+CMakeFiles/mygame.dir/engine/entity/player/playerMovement/simplePlayerMovement/SimplePlayerMovement.cpp.o:
+
+CMakeFiles/mygame.dir/engine/fps/Fps.cpp.o:
+
+CMakeFiles/mygame.dir/engine/input/InputManager.cpp.o:
+
+CMakeFiles/mygame.dir/engine/renderer/Renderer.cpp.o:
+
+CMakeFiles/mygame.dir/engine/renderer/camera/Camera.cpp.o:
+
+CMakeFiles/mygame.dir/game/Game.cpp.o:
+
+CMakeFiles/mygame.dir/src/main.cpp.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libSDL2.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libSDL2_ttf.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libSDL2.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libSDL2_ttf.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libstdc++.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libm.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libm.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libm.so:
+
+/usr/lib/libm.so.6:
+
+/usr/lib/libmvec.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libc.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libc.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libc.so:
+
+/usr/lib/libc.so.6:
+
+/usr/lib/libc_nonshared.a:
+
+/usr/lib/ld-linux-x86-64.so.2:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/crtendS.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/crtn.o:
+
+/usr/lib32/libfreetype.so.6:
+
+/usr/lib/libfreetype.so.6:
+
+/usr/lib32/libharfbuzz.so.0:
+
+/usr/lib/libharfbuzz.so.0:
+
+/usr/lib/ld-linux-x86-64.so.2:
+
+/usr/lib32/libz.so.1:
+
+/usr/lib/libz.so.1:
+
+/usr/lib32/libbz2.so.1.0:
+
+/usr/lib/libbz2.so.1.0:
+
+/usr/lib32/libpng16.so.16:
+
+/usr/lib/libpng16.so.16:
+
+/usr/lib32/libbrotlidec.so.1:
+
+/usr/lib/libbrotlidec.so.1:
+
+/usr/lib32/libglib-2.0.so.0:
+
+/usr/lib/libglib-2.0.so.0:
+
+/usr/lib/libgraphite2.so.3:
+
+/usr/lib32/libbrotlicommon.so.1:
+
+/usr/lib/libbrotlicommon.so.1:
+
+/usr/lib32/libpcre2-8.so.0:
+
+/usr/lib/libpcre2-8.so.0:
