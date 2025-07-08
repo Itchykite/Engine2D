4748dd02bbfe25582449c71e2c1f198053b0dddf
Kacper <cyrankowskikacper123@gmail.com>
2025-07-08 08:51:42 +0200

    Deleted build catalog

--- build/CMakeFiles/mygame.dir/engine/renderer/camera/Camera.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/mygame.dir/engine/renderer/camera/Camera.cpp.o: \
- /home/kacper/cpp/Engine2D/engine/renderer/camera/Camera.cpp \
- /usr/include/stdc-predef.h \
- /home/kacper/cpp/Engine2D/engine/renderer/camera/Camera.hpp
